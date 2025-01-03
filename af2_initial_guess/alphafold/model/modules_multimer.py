a68bd7b5b49516f883c05c997ad9005583e21258
Ben Orr <bendorr2415@gmail.com>
2025-01-02 23:53:57 -0500

    change deletion_mean_value size

--- af2_initial_guess/alphafold/model/modules_multimer.py
+++ af2_initial_guess/alphafold/model/modules_multimer.py
@@ -218,7 +218,7 @@ def create_msa_feat(batch):
   ### Ben Orr 1.2.25: Setting a dummy deletion_mean_value
   ### [..., None] adds a dimension to an array
   # deletion_mean_value = np.zeros(batch['msa'].shape, dtype=np.float32)[..., None] # I believe this caused msa_feat to have shape (27, 256) instead of (49, 256)
-  deletion_mean_value = np.zeros((22,256)) # it appears that I need an extra (22,256) zeros to match n_params for alphafold/alphafold_iteration/evoformer/preprocess_msa/weights
+  deletion_mean_value = np.zeros((229,22)) # it appears that I need an extra (229,22) zeros to match n_params for alphafold/alphafold_iteration/evoformer/preprocess_msa/weights
 
   ### Ben Orr 1.2.25: Setting a dummy batch['cluster_profile']
   batch['cluster_profile'] = np.zeros(batch['msa'].shape, dtype=np.float32)[..., None]
